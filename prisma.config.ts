efa41bee785b61c255b50d8f9280cb2e80adbbbf
deepblue-dot <deepblue5991@gmail.com>
2025-09-03 20:09:46 +0300

    final fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "prisma/config";
 import path from "node:path";
+import dotenv from "dotenv";
 
-process.loadEnvFile();
+dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 
 export default defineConfig({
   schema: path.join("prisma", "schema.prisma"),
