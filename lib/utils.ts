e2c4056634a65fdab20b362eb75ea03db592a172
deepblue-dot <deepblue5991@gmail.com>
2025-09-07 21:33:04 +0300

    update

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,25 +1,12 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 import formidable from "formidable";
-
 import { Decimal } from "@prisma/client/runtime/library"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export async function parseForm(req: Request) {
-  const form = formidable({ multiples: false });
-
-  return new Promise<{ fields: formidable.Fields; files: formidable.Files }>(
-    (resolve, reject) => {
-      form.parse(req as any, (err, fields, files) => {
-        if (err) reject(err);
-        resolve({ fields, files });
-      });
-    }
-  );
-}
 
 export async function parseForm(req: Request) {
   const form = formidable({ multiples: false });
